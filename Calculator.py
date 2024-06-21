3b05f0edd5ba7d23d1bc08d772ce914caec2159b
Balaji R <146870775+CodeExplorer-23@users.noreply.github.com>
2024-06-21 17:19:53 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,14 @@
 import tkinter as tk
 import math
 
+
 root = tk.Tk()
-root.title("Calc")
+root.title("Calculator")
+
+icon_path = "icon.png" 
+icon = tk.PhotoImage(file=icon_path)
+root.iconphoto(False, icon)
+
 
 root.mainloop()
 
